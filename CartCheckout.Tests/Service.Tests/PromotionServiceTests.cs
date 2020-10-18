0f56512c6561ade89a093d85952b430877215805
Rajat Basera <RajatBas@youforce.net>
2020-10-18 14:13:23 +0530

    Added Promotion service unit test

--- CartCheckout.Tests/Service.Tests/PromotionServiceTests.cs
+++ CartCheckout.Tests/Service.Tests/PromotionServiceTests.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriceCalculator.Models;
+using PriceCalculator.Service;
+using System.Collections.Generic;
 
 namespace CartCheckout.Tests
 {
@@ -6,8 +9,31 @@ namespace CartCheckout.Tests
     public class PromotionServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PromotionService_GetSingleProductPromotions_ShouldReturnAllSingleProductPromotion()
         {
+            var testData = new List<SingleProductPromotion>
+            {
+                new SingleProductPromotion{ PromotionId=1, Quantity=3, SKU="A", PromotionPrice=130 },
+                new SingleProductPromotion{ PromotionId=2, Quantity=2, SKU="B", PromotionPrice=45 }
+            };
+            var productService = new PromotionsService();
+            var result = productService.GetSingleProductPromotions();
+
+            Assert.AreEqual(testData[0].PromotionPrice, result[0].PromotionPrice);
+            Assert.AreEqual(testData[1].PromotionPrice, result[1].PromotionPrice);
+        }
+
+        [TestMethod]
+        public void PromotionService_GetMultiProductPromotions_ShouldReturnAllMultiProductPromotion()
+        {
+            var testData = new List<MultiProductPromotion>
+            {
+               new MultiProductPromotion{ PromotionId=1, Quantity=3, SKU=new List<string>{"C","D"}, PromotionPrice=30 }
+            };
+            var productService = new PromotionsService();
+            var result = productService.GetMultiProductPromotions();
+
+            Assert.AreEqual(result[0].PromotionPrice, testData[0].PromotionPrice);
         }
     }
 }
