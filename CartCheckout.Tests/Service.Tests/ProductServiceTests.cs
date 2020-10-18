8afe43c5f875adf0acde21b40eeafb43e4d19f2d
Rajat Basera <RajatBas@youforce.net>
2020-10-18 13:52:00 +0530

    Added test for product service

--- CartCheckout.Tests/Service.Tests/ProductServiceTests.cs
+++ CartCheckout.Tests/Service.Tests/ProductServiceTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriceCalculator.Service;
 
 namespace CartCheckout.Tests
 {
@@ -6,8 +7,39 @@ namespace CartCheckout.Tests
     public class ProductServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ProductService_GetProductPrice_ShouldReturnPriceForProductA()
         {
+            var productService = new ProductService();
+            int result = productService.GetProductPrice("A");
+
+            Assert.AreEqual(result, 50); ;
+        }
+
+        [TestMethod]
+        public void ProductService_GetProductPrice_ShouldReturnPriceForProductB()
+        {
+            var productService = new ProductService();
+            int result = productService.GetProductPrice("B");
+
+            Assert.AreEqual(result, 30); ;
+        }
+
+        [TestMethod]
+        public void ProductService_GetProductPrice_ShouldReturnPriceForProductC()
+        {
+            var productService = new ProductService();
+            int result = productService.GetProductPrice("C");
+
+            Assert.AreEqual(result, 20); ;
+        }
+
+        [TestMethod]
+        public void ProductService_GetProductPrice_ShouldReturnPriceForProductD()
+        {
+            var productService = new ProductService();
+            int result = productService.GetProductPrice("D");
+
+            Assert.AreEqual(result, 15); ;
         }
     }
 }
