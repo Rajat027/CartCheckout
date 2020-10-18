d6ce248b67b80c680ff0298956f0914a1192e56c
Rajat Basera <RajatBas@youforce.net>
2020-10-18 13:07:27 +0530

    Added empty project

---
+++ CartCheckout.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CartCheckout.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
